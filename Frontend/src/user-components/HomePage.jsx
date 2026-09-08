edc7cf835052c4b0a63a82587a2b30ffbe691ea0
Samuel Olaseinde-isaac <samuelolaseinde14@gmail.com>
2026-09-08 09:28:05 +0100

    feat: implement error handling middleware for URL shortener form

---
+++ Frontend/src/user-components/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ShortenerForm from "./ShortenerForm";
+import { useActionData, useNavigation } from "react-router";
+import { createUrl } from "./user-api";
+
+export async function action({ request }) {
+  try {
+    const formData = await request.formData();
+    const originalUrl = formData.get("originalUrl");
+    const newUrl = await createUrl({ originalUrl });
+    return newUrl;
+  } catch (error) {
+    return error;
+  }
+}
+
+export default function HomePage() {
+  const newUrl = useActionData();
+  const navigation = useNavigation();
+  return (
+    <div className="component-container">
+      <ShortenerForm newUrl={newUrl} navigation={navigation} />
+    </div>
+  );
+}
