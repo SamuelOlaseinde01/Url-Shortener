0d7ad76073eed5aeb16c3be41fcfa071e0e1eca0
Samuel Olaseinde-isaac <samuelolaseinde14@gmail.com>
2026-09-11 11:11:12 +0100

    feat: create login and register routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,13 +9,20 @@ import Layout from "./Layout";
 import LinkDetailsPage from "./user-components/LinkDetailsPage";
 import HomePage, { action as homeAction } from "./user-components/HomePage";
 import "./style.css";
+import Login from "./user-components/Login";
+import Register from "./user-components/Register";
 
 export default function App() {
   const router = createBrowserRouter(
     createRoutesFromChildren(
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} action={homeAction} />
-        <Route path="/:id" element={<LinkDetailsPage />} />
+      <Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} action={homeAction} />
+          <Route path="/:id" element={<LinkDetailsPage />} />
+        </Route>
       </Route>
     )
   );
