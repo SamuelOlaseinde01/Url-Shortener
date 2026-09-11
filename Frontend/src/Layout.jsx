0880e385b7afeddac6f4ec201bf25d9a2559f78d
Samuel Olaseinde-isaac <samuelolaseinde14@gmail.com>
2026-09-11 20:54:58 +0100

    feat: update Layout to display dynamic user greetings

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-import { Link, Outlet } from "react-router";
+import { Link, Outlet, useLoaderData } from "react-router";
 import Profile from "./user-components/Profile";
+import { getOptionalUser } from "./user-components/user-api";
 
-export default function Layout({ isLoggedIn, user }) {
+export async function loader() {
+  const user = await getOptionalUser();
+  return user;
+}
+
+export default function Layout() {
+  const user = useLoaderData();
+  console.log(user);
   return (
     <>
       <header>
-        {isLoggedIn ? <h2>Welcome {user.firstName}</h2> : <h2>ClipLink</h2>}
+        {user ? (
+          <h2 style={{ textTransform: "capitalize" }}>
+            Welcome, {user.firstName}
+          </h2>
+        ) : (
+          <h2>ClipLink</h2>
+        )}
 
-        {isLoggedIn ? (
+        {user ? (
           <nav>
             <Profile />
           </nav>
