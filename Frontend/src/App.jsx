2508a8d1434917cc62ba64d2f7a3d7fffa5b04c5
Samuel Olaseinde-isaac <samuelolaseinde14@gmail.com>
2026-09-11 20:54:03 +0100

    feat: add loaders and actions to login and register route - integrate authentication loaders into React Router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,23 +5,32 @@ import {
   Route,
   RouterProvider,
 } from "react-router";
-import Layout from "./Layout";
+import Layout, { loader as layoutLoader } from "./Layout";
 import LinkDetailsPage from "./user-components/LinkDetailsPage";
 import HomePage, { action as homeAction } from "./user-components/HomePage";
 import "./style.css";
-import Login from "./user-components/Login";
-import Register from "./user-components/Register";
+import Login, { action as loginAction } from "./user-components/Login";
+import Register, { action as registerAction } from "./user-components/Register";
+import { requireAuth } from "./user-components/user-api";
 
 export default function App() {
   const router = createBrowserRouter(
     createRoutesFromChildren(
       <Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/login" action={loginAction} element={<Login />} />
+        <Route
+          path="/register"
+          action={registerAction}
+          element={<Register />}
+        />
         <Route />
-        <Route path="/" element={<Layout />}>
+        <Route path="/" loader={layoutLoader} element={<Layout />}>
           <Route index element={<HomePage />} action={homeAction} />
-          <Route path="/:id" element={<LinkDetailsPage />} />
+          <Route
+            path="/:id"
+            loader={requireAuth}
+            element={<LinkDetailsPage />}
+          />
         </Route>
       </Route>
     )
