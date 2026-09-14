8559e29f6736886eaecd1294573f0a01a2c5c6a2
Samuel Olaseinde-isaac <samuelolaseinde14@gmail.com>
2026-09-14 22:51:25 +0100

    refactor: Wrap ShortenerForm content in a container div

--- Frontend/src/user-components/ShortenerForm.jsx
+++ Frontend/src/user-components/ShortenerForm.jsx
@@ -24,48 +24,50 @@ export default function ShortenerForm({ newUrl, navigation }) {
   }
 
   return (
-    <div className="url-form-container">
-      <div className="url-heading-container">
-        <h2>URL Shortener</h2>
-      </div>
-      <Form method="post" className="url-form">
-        <input
-          type="text"
-          name="originalUrl"
-          placeholder="Enter the link here"
-          required
-        />
-        {activeErrorMessage && (
-          <p className="error-text">{activeErrorMessage}</p>
-        )}
-        <button
-          className={
-            navigation.state === "submitting"
-              ? "url-submitting-btn"
-              : "url-submit-btn"
-          }
-        >
-          {navigation.state === "submitting" ? "Submitting" : "Submit"}
-        </button>
-      </Form>
-      {shortenedUrl && (
-        <span className="shortened-link-container">
-          Shortended Url:
-          <a href={shortenedUrl} target="_blank">
-            {`http://localhost:3000/${shortenedUrl}`}
-          </a>
-          {isCopied ? (
-            <Check color="blue" size={17} cursor={"pointer"} />
-          ) : (
-            <Copy
-              color="blue"
-              size={17}
-              cursor={"pointer"}
-              onClick={handleCopy}
-            />
+    <div className="shortener-form">
+      <div className="url-form-container">
+        <div className="url-heading-container">
+          <h2>URL Shortener</h2>
+        </div>
+        <Form method="post" className="url-form">
+          <input
+            type="text"
+            name="originalUrl"
+            placeholder="Enter the link here"
+            required
+          />
+          {activeErrorMessage && (
+            <p className="error-text">{activeErrorMessage}</p>
           )}
-        </span>
-      )}
+          <button
+            className={
+              navigation.state === "submitting"
+                ? "url-submitting-btn"
+                : "url-submit-btn"
+            }
+          >
+            {navigation.state === "submitting" ? "Submitting" : "Submit"}
+          </button>
+        </Form>
+        {shortenedUrl && (
+          <span className="shortened-link-container">
+            Shortended Url:
+            <a href={shortenedUrl} target="_blank">
+              {`http://localhost:3000/${shortenedUrl}`}
+            </a>
+            {isCopied ? (
+              <Check color="blue" size={17} cursor={"pointer"} />
+            ) : (
+              <Copy
+                color="blue"
+                size={17}
+                cursor={"pointer"}
+                onClick={handleCopy}
+              />
+            )}
+          </span>
+        )}
+      </div>
     </div>
   );
 }
