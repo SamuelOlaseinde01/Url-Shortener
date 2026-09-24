0a610c2743657c9a9510be9d79f8bd96c8d485bd
Samuel Olaseinde-isaac <samuelolaseinde14@gmail.com>
2026-09-24 20:14:03 +0100

    fix: add url middleware and editUrl controller to router

--- Backend/routers/url.js
+++ Backend/routers/url.js
@@ -5,21 +5,26 @@ const {
   createUrl,
   deleteUrl,
   claimGuestUrls,
+  editUrl,
 } = require("../controllers/urlfunctions");
+
 const {
   requireAuthMiddleware,
   optionalAuthMiddleware,
 } = require("../middleware/authMiddleware");
+
+const urlMiddleware = require("../middleware/urlMiddleware");
 
 const router = express.Router();
 
 router
   .route("/")
   .get(requireAuthMiddleware, getAllUrls)
-  .post(optionalAuthMiddleware, createUrl);
+  .post(optionalAuthMiddleware, urlMiddleware, createUrl);
 router
   .route("/:id")
   .get(requireAuthMiddleware, getUrl)
+  .patch(requireAuthMiddleware, urlMiddleware, editUrl)
   .delete(requireAuthMiddleware, deleteUrl);
 router.route("/claim").post(requireAuthMiddleware, claimGuestUrls);
 
